bb3c64ca049cb4a61dbf6292742f34a3de43f73c
tropicaliahomeau <tu-email-de-github@ejemplo.comgit config --global user.name>
2026-03-05 10:27:39 +1100

    Fix: Add missing Image import in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import styles from './Navbar.module.css';
